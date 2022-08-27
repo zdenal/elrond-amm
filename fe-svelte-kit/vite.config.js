1e683db486486f9d61ab0a4b272099281cfa597b
zdenal <nevralaz@gmail.com>
2022-08-27 21:38:12 +0200

    Let finally nodes issues work (Buffer one and so).

--- fe-svelte-kit/vite.config.js
+++ fe-svelte-kit/vite.config.js
@@ -1,10 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
+// Config is based on metaplex + vite example from:
+// https://github.com/metaplex-foundation/js-examples/tree/main/getting-started-vite
+
+// es2020 Needed for BigNumbers
+// See https://github.com/sveltejs/kit/issues/859
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	define: {
-		global: {}
+		global: {},
+		'process.env': {}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+			target: 'es2020'
+		}
 	}
 };
 
