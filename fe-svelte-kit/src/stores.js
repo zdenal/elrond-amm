2151f53e95f1bcf01ddf53ae84eecd4015c438b5
zdenal <nevralaz@gmail.com>
2022-11-18 10:48:57 +0100

    Persistent connection.

--- fe-svelte-kit/src/stores.js
+++ fe-svelte-kit/src/stores.js
@@ -1,4 +1,13 @@
 import { writable } from 'svelte/store';
 
-export const provider = writable();
 export const contractData = writable();
+
+const storedProvider = typeof localStorage !== 'undefined' && localStorage.getItem('provider');
+console.log(storedProvider);
+export const provider = writable(JSON.parse(storedProvider));
+
+if (typeof localStorage !== 'undefined') {
+	provider.subscribe((provider) => {
+		localStorage.setItem('provider', JSON.stringify(provider) || '');
+	});
+}
